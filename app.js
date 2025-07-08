3fc751964bc435ceb6d69dd07854379b6a3fab38
Akash Odedara <akashodedara7@gmail.com>
2025-07-09 00:11:40 +0530

    Day 2: Updated Express server code

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // EP -16 : Setting up a basic Express server
 // This code sets up a simple Express server that listens on port 3000 and responds with        
 
-const express = require("express")
+const express = require("express");
 
 const app = express();
+// app.use("/", (req, res) => {
+    
+//     res.send("Hello from the use middleware!");
+// });
 
-app.get("/", (req, res) => {
+app.get("/user", (req, res) => {
     res.send("Hello from the home!");
-}); 
+});
 
-app.get("/test", (req, res) => {
+app.post("/user", (req, res) => {
     res.send("Hello from the test!");
-}); 
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Hello from the patch!");
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");  
+    console.log("Server is running on port 3000");
 });
+
 
 
 
