2d549b8d5a17c66a09e9d253e70e55bbd4623ecb
Akash Odedara <akashodedara7@gmail.com>
2025-07-14 18:36:16 +0530

    auth- middlware

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// EP -16 : Setting up a basic Express server
-// This code sets up a simple Express server that listens on port 3000 and responds with        
 
 const express = require("express");
-
 const app = express();
-app.use("/user", (req, res ,next) => {
-    console.log("This is the first middleware function");
-    next();
-    res.send("Hello from the use middleware!");
-    
-},
- (req, res, next) =>{
-    console.log("This is the second middleware function");
-    res.send("Hello from the second middleware!");
-    next();     
- });
+
+const  {adminAuth} = require{"./middleware/auth"};
+
+
+app.use("/admin", adminAuth);
+app.get("/admin/getdata", (req, res, next) =>{
+
+    app.send("data sent to you");
+})
+app.get("/admin/delete", (req, res, next) =>{
+
+    app.send("data deleted ");
+})
 
 
 app.listen(3000, () => {
