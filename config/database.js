70383900ca4818f21c689df391b970ee82ed2c14
Akash Odedara <akashodedara7@gmail.com>
2025-09-01 17:43:39 +0530

    Remove outdated files and routes related to user authentication and profile management, including package.json, validation utilities, and request handling. This cleanup prepares the codebase for a new architecture or feature set.

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://Meet-Dev:C7SJdEcDU0SWaGgx@meet-dev.ll2bgjx.mongodb.net/Meet-dev");
-};
-
-module.exports =  connectDB;
